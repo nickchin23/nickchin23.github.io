9eae072733387b766f6a93e2f83f29bc55805094
nickchin23 <nicholas.chin23@gmail.com>
2019-01-20 00:45:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function storybutton() {
+  var x = document.getElementById("story");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function remedybutton() {
+  var y = document.getElementById("remedy");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
+function testimonybutton() {
+  var w = document.getElementById("test");
+  if (w.style.display === "block") {
+    w.style.display = "none";
+  } else {
+    w.style.display = "block";
+  }
+}
